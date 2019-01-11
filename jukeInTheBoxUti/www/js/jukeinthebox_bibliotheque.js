c8f663b524d55a6bb207ac84bd576a2cf98b5a9c
Camille SCHWARZ <camille.schwarz9@etu.univ-lorraine.fr>
2019-01-11 10:17:45 +0100

    Résolution conflit fichier js

--- jukeInTheBoxUti/www/js/jukeinthebox_bibliotheque.js
+++ jukeInTheBoxUti/www/js/jukeinthebox_bibliotheque.js
@@ -1,33 +1,33 @@
 function recupBibliotheque() {
-    $.ajax({
-      url: "https://webetu.iutnc.univ-lorraine.fr/www/rimet2u/jukeinthebox/catalogue.php",
-      context: document.body,
-      headers: {
-        "Authorization": "Basic " + btoa("rimet2u:070998.A")
-      }
-    }).done(function (json) {
-      for (var i=0; i<json["catalogue"]["pistes"].length; i++){
-        $(".bibliotheque").append(descPiste(json["catalogue"]["pistes"][i]));
-       }
-    });
-  }
+  $.ajax({
+    url: "https://webetu.iutnc.univ-lorraine.fr/www/rimet2u/jukeinthebox/catalogue.php",
+    context: document.body,
+    headers: {
+      "Authorization": "Basic " + btoa("rimet2u:070998.A")
+    }
+  }).done(function (json) {
+    for (var i = 0; i < json["catalogue"]["pistes"].length; i++) {
+      $(".bibliotheque").append(descPiste(json["catalogue"]["pistes"][i]));
+    }
+  });
+}
 
-  function descPiste(piste){
-    let desc= "<div class='piste'>";
-    desc += "<img class ='img_piste' src='" + piste["imagePiste"]+ "' />";
-    desc += "<p>" 
-    let taille = piste["artistes"].length;
-    if (taille == 1) {
-    desc += piste["artistes"][0]["prénom"] +" "+ piste["artistes"][0]["nom"];
+function descPiste(piste) {
+  let desc = "<div class='piste'>";
+  desc += "<img class ='img_piste' src='" + piste["imagePiste"] + "' />";
+  desc += "<p>"
+  let taille = piste["artistes"].length;
+  if (taille == 1) {
+    desc += piste["artistes"][0]["prénom"] + " " + piste["artistes"][0]["nom"];
+  }
+  else {
+    desc += piste["artistes"][0]["prénom"] + " " + piste["artistes"][0]["nom"];
+    for (var i = 1; i < taille; i++) {
+      desc += " / " + piste["artistes"][i]["prénom"] + " " + piste["artistes"][i]["nom"];
     }
-    else{
-        desc += piste["artistes"][0]["prénom"] +" "+ piste["artistes"][0]["nom"];
-        for(var i=1; i<taille; i++) {
-            desc += " / " + piste["artistes"][i]["prénom"] +" "+ piste["artistes"][i]["nom"];
-        }
-    }
-    desc += " - " + piste["nomPiste"] + " (" + piste["annéePiste"] + ") </p>";
-      desc += "<button class='add_button' type='submit' onclick='addFile(" + piste["idPiste"] + ")'>Ajouter à la file</button>";
+  }
+  desc += " - " + piste["nomPiste"] + " (" + piste["annéePiste"] + ") </p>";
+  desc += "<button class='add_button' type='submit' onclick='addFile(" + piste["idPiste"] + ")'>Ajouter à la file</button>";
   desc += "</div>";
   desc += "<hr>";
   return desc;
@@ -44,7 +44,10 @@ function addFile(idPiste) {
     headers: {
       "Authorization": "Basic " + btoa("rimet2u:070998.A")
     }
+  }).done(function () {
+    let lien = window.location.toString().split("bibliotheque.html");
+    window.location = lien[0] + 'file.html';
   });
 }
 
-  recupBibliotheque();
+recupBibliotheque();

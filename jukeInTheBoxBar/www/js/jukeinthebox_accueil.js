3dc43199b20ec7f8668003a100738dc4db286b48
Maeva <butaye.maeva@gmail.com>
2019-01-11 14:02:34 +0100

    add première pages de l'appli barman

---
+++ jukeInTheBoxBar/www/js/jukeinthebox_accueil.js
@@ -0,0 +1,50 @@
+let music;
+let idmusic;
+
+function recupFile() {
+    $(document).ajaxStart(function(){
+        $("#wait").css("display", "block");
+      });
+      
+      $(document).ajaxComplete(function(){
+        $("#wait").css("display", "none");
+      });
+      
+    $.ajax({
+      url: "https://webetu.iutnc.univ-lorraine.fr/www/rimet2u/jukeinthebox/index.php",
+      context: document.body,
+      headers: {
+        "Authorization": "Basic " + btoa("rimet2u:070998.A")
+      }
+    }).done(function (data) {
+      let json = JSON.parse(data);
+      if (json["pistes"][0] != null) {
+        if (idmusic != json["pistes"][0]["idFile"]) {
+            idmusic = json["pistes"][0]["idFile"];
+           }
+           descPiste(json["pistes"][0]["piste"]);
+      }
+      else $(".musique_en_cours").html("<div class='info_musique'> <p>Musique dans la file : Aucune</p> </div>");
+    });
+  }
+
+  function descPiste(piste) {
+    let desc = "<p><h2>Titre :</h2> " + piste["nom"] + "</p>";
+    desc += "<p><h2>Artiste :</h2> ";
+    let taille = piste["artistes"].length;
+    if (taille == 1) {
+        desc += piste["artistes"][0]["prénom"] +" "+ piste["artistes"][0]["nom"];
+    }
+    else{
+        desc += piste["artistes"][0]["prénom"] +" "+ piste["artistes"][0]["nom"];
+        for(var i=1; i<taille; i++) {
+            desc += " / " + piste["artistes"][i]["prénom"] +" "+ piste["artistes"][i]["nom"];
+        }
+    }
+    desc += "</p>";
+    desc += "<p><h2>Album : </h2>" + piste["albums"][0]["nom"] + "</p>";
+    $(".musique_en_cours").append("<img class='img_musique' src='"+piste["image"]+"' width='150px' />");
+    $(".musique_en_cours").append("<div class='info_musique'>" + desc + "</div>");
+  }
+
+  recupFile();
---
+++ jukeInTheBoxBar/www/js/jukeinthebox_file.js
@@ -0,0 +1,45 @@
+function recupFile() {
+  $(document).ajaxStart(function () {
+    $("#wait").css("display", "block");
+  });
+
+  $(document).ajaxComplete(function () {
+    $("#wait").css("display", "none");
+  });
+
+  $.ajax({
+    url: "https://webetu.iutnc.univ-lorraine.fr/www/rimet2u/jukeinthebox/index.php",
+    context: document.body,
+    headers: {
+      "Authorization": "Basic " + btoa("rimet2u:070998.A")
+    }
+  }).done(function (data) {
+    let compteur = 1;
+    let json = JSON.parse(data);
+    if (json["pistes"][0] != null) {
+      for (var i = 0; i < json["pistes"].length; i++) {
+        $(".file_piste").append("<p class='musique_file'>" + compteur + descPiste(json["pistes"][i]["piste"]));
+        compteur++;
+      }
+    }
+    else $(".file_piste").html("<p class='musique_file'>Musique dans la file : Aucune</p>");
+  });
+}
+
+function descPiste(piste) {
+  let desc = ") ";
+  let taille = piste["artistes"].length;
+  if (taille == 1) {
+    desc += piste["artistes"][0]["prénom"] + " " + piste["artistes"][0]["nom"];
+  }
+  else {
+    desc += piste["artistes"][0]["prénom"] + " " + piste["artistes"][0]["nom"];
+    for (var i = 1; i < taille; i++) {
+      desc += " / " + piste["artistes"][i]["prénom"] + " " + piste["artistes"][i]["nom"];
+    }
+  }
+  desc += " - " + piste["nom"] + "</p>";
+  return desc;
+}
+
+recupFile();

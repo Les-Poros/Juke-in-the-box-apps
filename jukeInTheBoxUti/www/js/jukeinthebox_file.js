57c08f8412d0d20ae6f723957b337dff387511ce
Camille SCHWARZ <camille.schwarz9@etu.univ-lorraine.fr>
2019-02-12 14:36:08 +0100

    Modification de la file

--- jukeInTheBoxUti/www/js/jukeinthebox_file.js
+++ jukeInTheBoxUti/www/js/jukeinthebox_file.js
@@ -1,4 +1,51 @@
-function recupFile() {
+var accueil = new Vue({
+  el: '.file',
+  data: {
+    tabMusiques: [],
+    estVideFile: true,
+  },
+  methods: {
+  },
+  computed: {
+  },
+  created() {
+    axios
+      .get('https://webetu.iutnc.univ-lorraine.fr/www/rimet2u/jukeinthebox/', {
+        context: document.body,
+        headers: {
+          "Authorization": "Basic " + btoa("rimet2u:070998.A")
+        }
+      })
+      .then((response) => {
+        if (response.data.pistes.length > 0) {
+          let json = response.data;
+          this.estVideFile = false;
+          for (var i = 0; i < json["pistes"].length; i++) {
+            let compteur = i + 1;
+            let piste = json["pistes"][i]["piste"];
+            let nomPiste = piste["nomPiste"];
+            let artiste = '';
+            if (piste["artistes"].length > 1) {
+              for (var j = 0; j < piste["artistes"].length; j++) {
+                if (j == (piste["artistes"].length - 1)) {
+                  artiste += piste["artistes"][j]["prénom"] + " " + piste["artistes"][j]["nom"] + " ";
+                }
+                else {
+                  artiste += piste["artistes"][j]["prénom"] + " " + piste["artistes"][j]["nom"] + " / ";
+                }
+              }
+            }
+            else {
+              artiste += piste["artistes"][0]["prénom"] + " " + piste["artistes"][0]["nom"] + " ";
+            }
+            this.tabMusiques.push(compteur + ") " + artiste + "- " + nomPiste);
+          }
+        }
+      });
+  }
+});
+
+/*function recupFile() {
   $(document).ajaxStart(function () {
     $("#wait").css("display", "block");
   });
@@ -42,4 +89,4 @@ function descPiste(piste) {
   return desc;
 }
 
-recupFile();
+recupFile();*/

a6caaf16b127ea2a6f8319461d1b786595f2542f
Leo Galassi <leo.galassi1@etu.univ-lorraine.fr>
2019-02-22 15:37:45 +0100

    Affichage du contenu de la bibliothèque

--- jukeInTheBoxBar/www/js/jukeinthebox_contentbibliotheque.js
+++ jukeInTheBoxBar/www/js/jukeinthebox_contentbibliotheque.js
@@ -3,6 +3,16 @@ var content_biblio = new Vue({
     data: {
      bibliotheque :'',
      titreBiblio : '',
+
+    musique: '',
+    estVide: true,
+    aPlusieursArtistes: false,
+    nomPiste: '',
+    albumPiste: '',
+    imagePiste: '',
+    artistesPiste: '',
+    prenomArtiste: '',
+    nomArtiste: '',
     },
     methods: {
     },
@@ -16,8 +26,26 @@ var content_biblio = new Vue({
         .then((response) => {
             console.log(response);
             if (response.data.catalogue.pistes.length > 0) {
+              //On récupère le titre de la bibliothèque
                 this.bibliotheque = response.data.catalogue.pistes[0].bibliotheques ;
                 this.titreBiblio = this.bibliotheque[0]['titre'];
+              //On récupère le contenu de la bibliothèque
+                this.musique = response.data.catalogue.pistes[0];
+                
+                this.estVide = false;
+                this.nomPiste = this.musique["nomPiste"];
+                this.albumPiste = this.musique["albums"][0]["nomAlbum"];
+                this.imagePiste = this.musique["imagePiste"];
+                
+                if (this.musique["artistes"].length > 1) {
+                  this.aPlusieursArtistes = true;
+                  this.artistesPiste = this.musique["artistes"];
+                }
+                else {
+                  this.prenomArtiste = this.musique["artistes"][0]["prénom"];
+                  this.nomArtiste = this.musique["artistes"][0]["nom"];
+                }
+                console.log(this.nomArtiste);
                 
             }
         });

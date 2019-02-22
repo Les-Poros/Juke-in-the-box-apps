0a08f7b706d6d66457ecd054938ef964583c1aef
Leo Galassi <leo.galassi1@etu.univ-lorraine.fr>
2019-02-22 15:20:48 +0100

    Affichage du Titre d'une bibliothèque du barman

--- jukeInTheBoxBar/www/js/jukeinthebox_contentbibliotheque.js
+++ jukeInTheBoxBar/www/js/jukeinthebox_contentbibliotheque.js
@@ -1,6 +1,7 @@
 var content_biblio = new Vue({
     el: '.bibliotheque',
     data: {
+     bibliotheque :'',
      titreBiblio : '',
     },
     methods: {
@@ -9,11 +10,16 @@ var content_biblio = new Vue({
     },
     created() {
       axios
-        .get('https://webetu.iutnc.univ-lorraine.fr/www/rimet2u/jukeinthebox/', {
+        .get('https://webetu.iutnc.univ-lorraine.fr/www/rimet2u/jukeinthebox/catalogue/1', {
           context: document.body,
         })
         .then((response) => {
-         
+            console.log(response);
+            if (response.data.catalogue.pistes.length > 0) {
+                this.bibliotheque = response.data.catalogue.pistes[0].bibliotheques ;
+                this.titreBiblio = this.bibliotheque[0]['titre'];
+                
+            }
         });
     }
   });

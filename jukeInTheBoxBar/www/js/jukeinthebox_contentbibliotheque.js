e3f2205c0152f26c8421f5cbd13c43db32e5de43
Leo Galassi <leo.galassi1@etu.univ-lorraine.fr>
2019-02-22 11:01:26 +0100

    Mise en place Axios pour Bibliothèque

--- jukeInTheBoxBar/www/js/jukeinthebox_contentbibliotheque.js
+++ jukeInTheBoxBar/www/js/jukeinthebox_contentbibliotheque.js
@@ -1,7 +1,19 @@
-$(document).ajaxStart(function () {
-    $("#wait").css("display", "block");
-});
-
-$(document).ajaxComplete(function () {
-    $("#wait").css("display", "none");
-});
+var content_biblio = new Vue({
+    el: '.bibliotheque',
+    data: {
+     titreBiblio : '',
+    },
+    methods: {
+    },
+    computed: {
+    },
+    created() {
+      axios
+        .get('https://webetu.iutnc.univ-lorraine.fr/www/rimet2u/jukeinthebox/', {
+          context: document.body,
+        })
+        .then((response) => {
+         
+        });
+    }
+  });

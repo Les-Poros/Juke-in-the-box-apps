c3589394aeb76f1cde2b9bf98d67a6b824788862
Leo Galassi <leo.galassi1@etu.univ-lorraine.fr>
2019-02-24 14:46:20 +0100

    Ajouter une musique dans la bibliothèqe V1 : Tout est en dur

--- jukeInTheBoxBar/www/js/jukeinthebox_addFileBibliotheque.js
+++ jukeInTheBoxBar/www/js/jukeinthebox_addFileBibliotheque.js
@@ -17,6 +17,20 @@ var bibliotheque = new Vue({
           this.listMusiques = response["data"]["catalogue"]["pistes"];
         });
       },
+      addFileBiblio : function(idPiste){
+        console.log(idPiste);
+      const params = new URLSearchParams();
+      params.append('id', idPiste);
+      params.append('bartender', token);
+      axios
+        .post("https://webetu.iutnc.univ-lorraine.fr/www/rimet2u/jukeinthebox/addFileBiblio", params)
+          .then(() => {
+            let lien = window.location.toString().split("bibliotheque.html");
+            window.location = lien[0] + 'accueil.html';
+            window.alert('Votre musique a été ajoutée à la file !');
+          });
+          
+      },
       // addFile : function(idPiste){
       //   console.log(idPiste);
       //   const params = new URLSearchParams();

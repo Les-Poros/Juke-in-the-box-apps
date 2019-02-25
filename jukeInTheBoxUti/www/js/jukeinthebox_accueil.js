4b621db36fe79a12f08439c75fd07d068d79c49a
Rimet <quentin.rimet2@etu.univ-lorraine.fr>
2019-02-25 17:57:55 +0100

    appli fonctionne que si on entre un qrcode

--- jukeInTheBoxUti/www/js/jukeinthebox_accueil.js
+++ jukeInTheBoxUti/www/js/jukeinthebox_accueil.js
@@ -1,4 +1,4 @@
-var accueil = new Vue({
+var content = new Vue({
   el: '.musique_en_cours',
   data: {
     musique: 'aucune',
@@ -7,67 +7,71 @@ var accueil = new Vue({
     aPlusieursAlbums: false,
     nomPiste: 'aucune',
     albumsPiste: '',
-    nomAlbum:'',
+    nomAlbum: '',
     imagePiste: '',
     artistesPiste: '',
     prenomArtiste: '',
     nomArtiste: '',
   },
   methods: {
-    getFirstFile : function(){
+    start: function () {
+      this.getFirstFile();
+      setInterval(function () { content.getFirstFile(); }, 15000);
+    },
+    getFirstFile: function () {
       axios
-      .get('https://webetu.iutnc.univ-lorraine.fr/www/rimet2u/jukeinthebox/File', {
-        context: document.body,
-        params:{
-          "token":token,
-          "first":true
-              },
-      })
-      .then((response) => {
-        if (response.data.pistes.length > 0) {
-          this.musique = response.data.pistes[0].piste;
-          this.estVide = false;
-          this.nomPiste = this.musique["nomPiste"];
-          this.albumPiste = this.musique["albums"][0]["nomAlbum"];
-          this.imagePiste = this.musique["imagePiste"];
-          if (this.musique["artistes"].length > 1) {
-            this.aPlusieursArtistes = true;
-            this.artistesPiste = this.musique["artistes"];
+        .get('https://webetu.iutnc.univ-lorraine.fr/www/rimet2u/jukeinthebox/File', {
+          context: document.body,
+          params: {
+            "token": token.token,
+            "first": true
+          },
+        })
+        .then((response) => {
+          if (response.data.pistes.length > 0) {
+            this.musique = response.data.pistes[0].piste;
+            this.estVide = false;
+            this.nomPiste = this.musique["nomPiste"];
+            this.albumPiste = this.musique["albums"][0]["nomAlbum"];
+            this.imagePiste = this.musique["imagePiste"];
+            if (this.musique["artistes"].length > 1) {
+              this.aPlusieursArtistes = true;
+              this.artistesPiste = this.musique["artistes"];
+            }
+            else {
+              this.aPlusieursArtistes = false;
+              this.prenomArtiste = this.musique["artistes"][0]["prénom"];
+              this.nomArtiste = this.musique["artistes"][0]["nom"];
+            }
+            if (this.musique["albums"].length > 1) {
+              this.aPlusieursAlbums = true;
+              this.albumsPiste = this.musique["albums"];
+            }
+            else {
+              this.aPlusieursAlbums = false;
+              this.nomAlbum = this.musique["albums"][0]["nomAlbum"];
+            }
           }
           else {
+            this.musique = 'aucune';
+            this.estVide = true;
             this.aPlusieursArtistes = false;
-            this.prenomArtiste = this.musique["artistes"][0]["prénom"];
-            this.nomArtiste = this.musique["artistes"][0]["nom"];
-          }
-          if (this.musique["albums"].length > 1) {
-            this.aPlusieursAlbums = true;
-            this.albumsPiste = this.musique["albums"];
-          }
-          else {
             this.aPlusieursAlbums = false;
-            this.nomAlbum = this.musique["albums"][0]["nomAlbum"];
+            this.nomPiste = 'aucune';
+            this.albumsPiste = '';
+            this.nomAlbum = '';
+            this.imagePiste = '';
+            this.artistesPiste = '';
+            this.prenomArtiste = '';
+            this.nomArtiste = '';
           }
-        }
-        else{
-          this.musique= 'aucune';
-          this.estVide= true;
-          this.aPlusieursArtistes= false;
-          this.aPlusieursAlbums= false;
-          this.nomPiste= 'aucune';
-          this.albumsPiste= '';
-          this.nomAlbum='';
-          this.imagePiste= '';
-          this.artistesPiste= '';
-          this.prenomArtiste= '';
-          this.nomArtiste= '';
-        }
-      });
+        });
     }
   },
   computed: {
+
   },
   created() {
-    this.getFirstFile();
-    setInterval(function(){accueil.getFirstFile();}, 15000);
+
   }
 });
--- jukeInTheBoxUti/www/js/token.js
+++ jukeInTheBoxUti/www/js/token.js
@@ -1,1 +1,42 @@
-var token="qrcode";
+
+var token = new Vue({
+    el: '.modal',
+    data: {
+      token:"",
+      unshow: true
+    },
+    methods: {
+        disparait : function(){
+            this.unshow=true;
+            localStorage.qrcode = this.token;
+            content.start();
+        },
+    getCookie : function(cname){
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+          }
+          if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+          }
+        }
+        return "";
+      }
+    },
+    computed: {
+        
+    },
+    created() {
+        if (localStorage.qrcode) {
+            this.token = localStorage.qrcode;
+          }
+          if(this.token == "")
+          this.unshow=false;
+          else 
+          window.onload=function(){content.start()};
+    }
+  });

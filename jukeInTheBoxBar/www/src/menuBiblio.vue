9202b599d1726edfb2815551f8fe89d2181ba9ae
Leo Galassi <leo.galassi1@etu.univ-lorraine.fr>
2019-03-27 13:11:40 +0100

    Affichage ajouter une musique dans biblio Barman est maintenant dynamique

--- jukeInTheBoxBar/www/src/menuBiblio.vue
+++ jukeInTheBoxBar/www/src/menuBiblio.vue
@@ -2,34 +2,25 @@
   <div class="">
     <h1>Juke in the box</h1>
   <ul class="navigation">
-  <li><button @click = "choix = biblio">Voir Sa bibliotheque</button></li>
-  <li><button @click = "choix = ajouter">Ajouter Une Musique</button></li>
-  <li><button @click = "choix = predef">Bibliothèque Prédéfinie</button></li>
+  <li><button @click = "choix = 'biblio'">Voir Sa bibliotheque</button></li>
+  <li><button @click = "choix = 'ajouter'">Ajouter Une Musique</button></li>
+  <li><button @click = "choix = 'predef'">Bibliothèque Prédéfinie</button></li>
   
 </ul>
-<bibliotheque v-if="choix==biblio" v-bind:apiurl="apiurl">
-
-  </bibliotheque>
-    <!-- <router-link to="/bibliotheque">
-      <div class="home_button">
-        <p>Voir sa bibliothèque</p>
-      </div>
-    </router-link>
-    <router-link to="/addBibliotheque">
-      <div class="home_button">
-        <p>Ajouter une musique</p>
-      </div>
-    </router-link> -->
+<bibliotheque v-if="choix=='biblio'" v-bind:apiurl="apiurl"> </bibliotheque>
+<ajouter v-if="choix=='ajouter'" v-bind:apiurl="apiurl"></ajouter>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import biblio from "./Bibliotheque.vue";
+import ajout from "./addBiblio.vue";
 export default {
   name: "menubiblio",
   components: {
-    'bibliotheque' : biblio
+    'bibliotheque' : biblio,
+    'ajouter' : ajout,
   },
   props: ["apiurl"],
   data() {
